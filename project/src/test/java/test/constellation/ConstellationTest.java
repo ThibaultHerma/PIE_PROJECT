9f7d660ccd7e86df6dacc76b2668142308736841
TheoNguyen611 <theo.nguyen14@gmail.com>
2020-11-16 22:47:44 +0100

    add test and json configuration

---
+++ project/src/test/java/test/constellation/ConstellationTest.java
@@ -0,0 +1,14 @@
+package test.constellation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConstellationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
