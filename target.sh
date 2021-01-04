df44fa6450d653b28d107e9365b7758bcd740009
TheoNguyen611 <71927239+TheoNguyen611@users.noreply.github.com>
2021-01-05 00:00:04 +0100

    add script to solve git issue with target folder (#42)
    * add script to solve git target issue

---
+++ target.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#  target.sh
+#  
+#
+#  Created by Théo Nguyen on 11/11/2020.
+#
+
+
+
+git checkout HEAD project/target/classes/constellation/README.md
+git checkout HEAD project/target/classes/decisionVector/README.md
+git checkout HEAD project/target/classes/optimisation/README.md
+git checkout HEAD project/target/classes/simulation/README.md
+git checkout HEAD project/target/classes/time/README.md
+git checkout HEAD project/target/classes/useCase/README.md
+git checkout HEAD project/target/classes/utils/README.md
+git checkout HEAD project/target/classes/zone/README.md
+git checkout HEAD project/target/test-classes/constellation/README.md
+git checkout HEAD project/target/test-classes/decisionVector/README.md
+git checkout HEAD project/target/test-classes/utils/README.md
+git checkout HEAD project/target/test-classes/zone/README.md
+
+git checkout HEAD project/.classpath
+
+rm project/.gitignore
